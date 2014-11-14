ef3f72fa45f33b3440c0bd87e66c2f138280c88e
Wouter de Vos <wouter@springest.com>
2014-11-15 00:15:53 +0100

    Basic post frontend views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.published.page(page_number).per(10)
-    render stream: true
   end
 
   def show
     @post = Post.published.friendly.find(params[:id])
-    render stream: true
   end
 
   private
