ade971d285a9ce6388ac3155fa31b8c6c5470a0b
Wouter de Vos <wouter@springest.com>
2014-11-15 00:04:43 +0100

    Frontend for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.page(page_number).per(10)
+    @posts = Post.published.page(page_number).per(10)
     render stream: true
   end
 
   def show
-    @post = Post.friendly.find(params[:id])
+    @post = Post.published.friendly.find(params[:id])
     render stream: true
   end
 
