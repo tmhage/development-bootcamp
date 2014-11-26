4cfca7af186a28454b4283623d2955edd6cd2805
Wouter de Vos <wouter@springest.com>
2014-11-26 13:21:56 +0100

    Adds website to safe sponsor params.

--- app/controllers/admin/sponsors_controller.rb
+++ app/controllers/admin/sponsors_controller.rb
@@ -48,6 +48,6 @@ class Admin::SponsorsController < Admin::AdminController
   def sponsor_params
     params[:sponsor][:activated_at] = Time.now if params[:sponsor][:activated_at] == '1'
     params.require(:sponsor).permit(:name, :description, :logo, :remarks, :email,
-      :hiring, :activated_at)
+      :hiring, :activated_at, :website)
   end
 end
