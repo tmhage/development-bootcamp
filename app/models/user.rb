e288ccf207ed0354ef649cb1375ea5a2af4f7697
Wouter de Vos <wouter@springest.com>
2014-11-28 13:51:26 +0100

    Proper blog post format, gravatars, the works.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, #:registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name, if: :persisted?
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
+  root to: "static_pages#about"
   get '/schedule' => 'static_pages#schedule'
   get '/tickets' => 'static_pages#tickets'
   get '/team' => 'static_pages#team'
-  get '/blog' => 'posts#index'
-  get '/blog/:id' => 'posts#show'
+
+  resources :blog, controller: 'posts', only: [:index, :show]
+  resources :sponsors, only: [:index, :new, :create]
 
   namespace :admin do
     resources :posts, except: :show do
@@ -16,9 +22,4 @@ Rails.application.routes.draw do
     end
     resources :sponsors, except: :show
   end
-
-  # resources :posts, only: [:index, :show]
-  resources :sponsors, only: [:index, :new, :create]
-
-  root to: "static_pages#about"
 end
