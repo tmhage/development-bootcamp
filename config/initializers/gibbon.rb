3bdc2125b66be597966b537f420fbcac10a7527b
Wouter de Vos <wouter@springest.com>
2014-12-02 11:36:33 +0100

    Added Mailchimp integration for sponsors.

---
+++ config/initializers/gibbon.rb
@@ -0,0 +1,9 @@
+Gibbon::API.api_key = ENV['DB_MAILCHIMP_API_KEY']
+Gibbon::API.timeout = 15
+Gibbon::API.throws_exceptions = false
+
+module MailingLists
+  STUDENTS = 'aff9cced90'
+  SPEAKERS = '37a3a54ac4'
+  SPONSORS = '6ce7b074db'
+end
