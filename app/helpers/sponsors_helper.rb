71dbff53bb0adf3699fe245ae217f05eeb4fc437
Wouter de Vos <wouter@springest.com>
2014-12-03 11:10:54 +0100

    Sponsor plans added and linked together.

--- app/helpers/sponsors_helper.rb
+++ app/helpers/sponsors_helper.rb
@@ -4,4 +4,10 @@ module SponsorsHelper
     max_updated_at = Sponsor.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "sponsors/all-#{count}-#{max_updated_at}"
   end
+
+  def sponsor_plans_collection
+    Sponsor.plans.map do |key, plan|
+      ["#{plan[:name]} #{number_to_currency(plan[:price])}", key]
+    end
+  end
 end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -8,4 +8,33 @@ class Sponsor < ActiveRecord::Base
   validates_uniqueness_of :name, scope: :email
 
   scope :active, -> { where.not(activated_at: nil) }
+
+  def self.plans
+    {
+      platinum: {
+        name: 'Platinum',
+        price: 10000
+      },
+      gold: {
+        name: 'Gold',
+        price: 5000
+      },
+      silver: {
+        name: 'Silver',
+        price: 2000
+      },
+      party: {
+        name: 'Party Support',
+        price: 5000
+      },
+      lunch: {
+        name: 'Lunch Support',
+        price: 5000
+      },
+      hackathon: {
+        name: 'Hackathon Support',
+        price: 2000
+      }
+    }
+  end
 end
