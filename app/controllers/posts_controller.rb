c3dd4c32b3071af62e63c115c2e927400a10cec0
Wouter de Vos <wouter@springest.com>
2014-12-10 13:41:35 +0100

    Fixing up posts index html, 404 page, and ordering Posts DESC by publish date.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.published.page(page_number).per(10)
+    @posts = Post.published.page(page_number).order('published_at DESC').per(10).page(page_number)
+    if page_number == 1 && params[:page].present?
+      redirect_to blog_index_path and return
+    end
+
+    if page_number > 1 && @posts.count == 0
+      page_not_found and return
+    end
   end
 
   def show
