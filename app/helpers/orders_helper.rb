2dba80bea12ca42d4c2b5884ba032f4b571e56f4
Wouter de Vos <wouter@springest.com>
2015-01-23 17:06:53 +0100

    Fix port issues in Mollie redirect urls.

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,8 +1,8 @@
 module OrdersHelper
   def payment_link_to(text, order, options = {})
     # We set the host and port here because Mollie does not accept local hosts.
-    payment_options = { redirectUrl: thanks_ticket_url(order, host: 'www.developmentbootcamp.nl', port: 80) }
-    payment_options[:webhookUrl] = webhook_tickets_url(host: 'www.developmentbootcamp.nl', port: 80)
+    payment_options = { redirectUrl: thanks_ticket_url(order, host: 'www.developmentbootcamp.nl') }
+    payment_options[:webhookUrl] = webhook_tickets_url(host: 'www.developmentbootcamp.nl')
     payment = order.create_payment payment_options
     link_to text, payment.getPaymentUrl, options
   end
