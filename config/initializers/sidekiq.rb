a68ddd589f6cc79d2c13839401b0b5f31c2f6486
Wouter de Vos <wouter@springest.com>
2015-06-14 17:20:37 +0200

    Counter situations where REDIS_URL is empty, such as when precompiling assets.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,4 +2,6 @@ require 'celluloid'
 require 'sidekiq'
 require 'sidekiq/processor'
 
-Sidekiq.redis = { url: ENV['REDIS_URL'] }
+if ENV['REDIS_URL'].present?
+  Sidekiq.redis = { url: ENV['REDIS_URL'] }
+end
