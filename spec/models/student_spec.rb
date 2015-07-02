538eed891ebcd36519d17dd52386808f21648c26
Wouter de Vos <wouter@springest.com>
2015-07-02 21:10:15 +0200

    Validate presence of Student phone number.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Student, :type => :model do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :email }
+  it { should validate_presence_of :phone_number }
 
   describe 'clean_twitter_handle' do
     let(:student) { build_stubbed(:student, twitter_handle: '@foo') }
