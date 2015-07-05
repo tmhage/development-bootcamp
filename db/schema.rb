9306223650a50e82515e9e33466ba34e487d62a7
Jovana <jovana@Jovanas-MacBook-Pro.local>
2015-07-05 18:33:08 +0200

    Added content on the program page

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 20150704084248) do
     t.uuid     "identifier"
     t.string   "stripe_token"
     t.json     "stripe_payload"
-    t.text     "qr_code"
     t.boolean  "terms_and_conditions"
     t.boolean  "manually_paid"
     t.boolean  "paid_by_creditcard"
