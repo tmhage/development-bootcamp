fbcd847cbd5e2b2667a5b424e532d12a15c08fcb
Wouter de Vos <wouter@springest.com>
2015-07-11 21:17:18 +0200

    Add rest of the participants to the newsletter.

---
+++ db/migrate/20150711191511_seed_more_students_to_mailing_list.rb
@@ -0,0 +1,40 @@
+class SeedMoreStudentsToMailingList < ActiveRecord::Migration
+  def change
+    return true unless Rails.env.production?
+    Order.all.each do |order|
+      order.students.each do |student|
+        add_to_list(student)
+      end
+    end
+  end
+
+  def add_to_list(student)
+    gb = Gibbon::API.new
+    gb.lists.subscribe({
+      id: MailingLists::PARTICIPANTS,
+      email: {
+        email: student.email
+      },
+      double_optin: false,
+      update_existing: true,
+      merge_vars: {
+        FNAME: student.first_name,
+        LNAME: student.last_name
+      }
+    })
+    gb.lists.subscribe({
+      id: MailingLists::STUDENTS,
+      email: {
+        email: student.email
+      },
+      double_optin: false,
+      update_existing: true,
+      merge_vars: {
+        FNAME: student.first_name,
+        LNAME: student.last_name
+      }
+    })
+  rescue Gibbon::MailChimpError => e
+    logger.error "Could not add #{student.email} to students mailing list: #{e.message}"
+  end
+end
