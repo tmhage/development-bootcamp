e34f17c4bff521f2becfe2d8b243f1711ad17064
Wouter de Vos <wouter@springest.com>
2015-09-09 12:01:47 +0200

    Do not load host rewrites unless we are on production

--- config/initializers/rewrites.rb
+++ config/initializers/rewrites.rb
@@ -1,8 +1,10 @@
 class DevelopmentBootcamp::Application < Rails::Application
-  config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{/(.*)}, "https://www.developmentbootcamp.nl/$1", if: Proc.new { |rack_env|
-      rack_env['SERVER_NAME'] != 'www.developmentbootcamp.nl'
-    }
+  if ::Rails.env.production?
+    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+      r301 %r{/(.*)}, "https://www.developmentbootcamp.nl/$1", if: Proc.new { |rack_env|
+        rack_env['SERVER_NAME'] != 'www.developmentbootcamp.nl'
+      }
+    end
   end
 end
 
