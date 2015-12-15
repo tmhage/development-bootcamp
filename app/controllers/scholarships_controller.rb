715ebb0d002de80fcc3ff958b6596792d37516d4
Wouter de Vos <wouter@codaisseur.com>
2015-12-15 20:55:03 +0100

    Add mixpanel tracking to scholarships

--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -3,12 +3,14 @@ class ScholarshipsController < ApplicationController
 
   def thanks
     session[:scholarship_application_id] ||= 0
+    mixpanel.track '[applies] Scholarship Application', scholarship_id: session[:scholarship_application_id]
     @scholarship = Scholarship.find(session[:scholarship_application_id])
     respond_with(@scholarship)
   end
 
   def apply
     @scholarship = Scholarship.new
+    mixpanel.track '[visits] Scholarship Application Page'
   end
 
   def create
