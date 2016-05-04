694b6cf07fdacfb3bf6751a46ee774a26d8d0ec3
Wouter de Vos <wouter@codaisseur.com>
2016-05-04 21:27:52 +0200

    AssetSync

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'mandrill-api'
 gem 'sidekiq'
 gem 'sinatra'
 gem 'route_translator'
+gem 'asset_sync'
 
 group :production, :staging do
   gem 'rails_12factor'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,9 @@ Rails.application.configure do
   }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//s3.amazonaws.com/development-bootcamp-assets"
+  config.assets.prefix = "/#{ENV['RAILS_ENV']}/assets"
+
   config.action_controller.default_url_options = {
     :host => "www.developmentbootcamp.nl"
   }
