9563c2a0f1676e6ac1fed86e682a483cf7ad8636
Wouter de Vos <wouter@codaisseur.com>
2016-05-04 17:39:02 +0200

    Re-enable the host redirect, Heroku works :tada:

--- config/initializers/rewrites.rb
+++ config/initializers/rewrites.rb
@@ -1,8 +1,8 @@
 class DevelopmentBootcamp::Application < Rails::Application
   config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    # r301 %r{/(.*)}, "https://www.developmentbootcamp.nl/$1", if: Proc.new { |rack_env|
-    #   ::Rails.env.production? && !!!(rack_env['SERVER_NAME'].match(/^www\.developmentbootcamp\.(nl|com)$/))
-    # }
+    r301 %r{/(.*)}, "https://www.developmentbootcamp.nl/$1", if: Proc.new { |rack_env|
+      ::Rails.env.production? && !!!(rack_env['SERVER_NAME'].match(/^www\.developmentbootcamp\.(nl|com)$/))
+    }
 
     # Rewrite old /program routes to /courses
     r301 '/program/level-1', '/courses/beginner-bootcamp'
